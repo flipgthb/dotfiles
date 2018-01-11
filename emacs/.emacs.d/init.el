93aa3938dc8763f02a4d6ef1311c7daa483062ec
Felippe Alves <flipgm@gmail.com>
2018-01-11 01:48:57 -0200

    Added helpful

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -2,8 +2,9 @@
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/")
-             '("org" . "http://orgmode.org/elpa/"))
+	     '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives
+             '("org" . "http://orgmode.org/elpa/") t)
 
 (package-initialize)
 
@@ -19,9 +20,15 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(indicate-empty-lines t)
+ '(org-default-notes-file (concat org-directory "notes.org"))
+ '(org-directory "~/Dropbox/orgfiles")
+ '(org-export-html-postamble nil)
+ '(org-hide-leading-stars t)
+ '(org-startup-folded (quote overview))
+ '(org-startup-indented t)
  '(package-selected-packages
    (quote
-    (org-ac web-mode elpy jedi htmlize ox-reveal yasnippet flycheck auto-complete iedit undo-tree hungry-delete expand-region mode-line-bell beacon org-bullets zenburn-theme github-modern-theme ace-window avy counsel try which-key dash use-package))))
+    (org org-plus-contrib org-ac web-mode elpy jedi htmlize ox-reveal yasnippet flycheck auto-complete iedit undo-tree hungry-delete expand-region mode-line-bell beacon org-bullets zenburn-theme github-modern-theme ace-window avy counsel try which-key dash use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
