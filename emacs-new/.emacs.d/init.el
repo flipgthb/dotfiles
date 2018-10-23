0342edb4b7ea77f78d86f4782e8d7e0b58706c7e
Felippe Alves <flipgm@gmail.com>
2018-10-23 18:40:55 -0300

    new emacs config

---
+++ emacs-new/.emacs.d/init.el
@@ -0,0 +1,47 @@
+;;; This fixed garbage collection, makes emacs start up faster ;;;;;;;
+(setq gc-cons-threshold 402653184
+      gc-cons-percentage 0.6)
+
+(defvar startup/file-name-handler-alist file-name-handler-alist)
+(setq file-name-handler-alist nil)
+
+(defun startup/revert-file-name-handler-alist ()
+  (setq file-name-handler-alist startup/file-name-handler-alist))
+
+(defun startup/reset-gc ()
+  (setq gc-cons-threshold 16777216
+	gc-cons-percentage 0.1))
+
+(add-hook 'emacs-startup-hook 'startup/revert-file-name-handler-alist)
+(add-hook 'emacs-startup-hook 'startup/reset-gc)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file 'noerror)
+
+;; package manager
+(require 'package)
+(setq package-enable-at-startup nil)
+(setq package-archives '(("ELPA"  . "http://tromey.com/elpa/")
+			 ("gnu"   . "http://elpa.gnu.org/packages/")
+			 ("melpa" . "https://melpa.org/packages/")
+			 ("org"   . "https://orgmode.org/elpa/")))
+			 ;; ("SC" . "http://joseito.republika.pl/sunrise-commander/")
+
+(package-initialize)
+
+
+;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package)
+	(package-refresh-contents)
+	(package-install 'use-package))
+
+
+(let ((fname (expand-file-name "~/.emacs.d/config.org")))
+  (if (file-exists-p fname)
+      (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))))
+
+(require 'server)
+(unless (server-running-p)
+  (server-start))
